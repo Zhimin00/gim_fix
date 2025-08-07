d61db8cf77cffb52c6583a26fb6c7db8f721f899
zhimin <zshao14@jh.edu>
2025-08-07 00:49:13 -0400

    inference

--- TEST_mast3r.sh
+++ TEST_mast3r.sh
@@ -3,7 +3,7 @@ gpus=$1
 weight=$2
 size=$3
 data_root=$4
-python test.py --gpus $gpus --weight $weight --version 100h --test --batch_size 1 --tests RobotcarWeather --img_size $size --DATA_ROOT $data_root
+python test.py --gpus $gpus --weight $weight --version 100h --test --batch_size 1 --tests           KITTI --img_size $size --DATA_ROOT $data_root
 python test.py --gpus $gpus --weight $weight --version 100h --test --batch_size 1 --tests          ETH3DO --img_size $size --DATA_ROOT $data_root
 python test.py --gpus $gpus --weight $weight --version 100h --test --batch_size 1 --tests         ICLNUIM --img_size $size --DATA_ROOT $data_root  
 python test.py --gpus $gpus --weight $weight --version 100h --test --batch_size 1 --tests        SceneNet --img_size $size --DATA_ROOT $data_root  
@@ -12,8 +12,8 @@ python test.py --gpus $gpus --weight $weight --version 100h --test --batch_size 
 python test.py --gpus $gpus --weight $weight --version 100h --test --batch_size 1 --tests        MultiFoV --img_size $size --DATA_ROOT $data_root 
 python test.py --gpus $gpus --weight $weight --version 100h --test --batch_size 1 --tests   RobotcarNight --img_size $size --DATA_ROOT $data_root  
 python test.py --gpus $gpus --weight $weight --version 100h --test --batch_size 1 --tests  RobotcarSeason --img_size $size --DATA_ROOT $data_root  
+python test.py --gpus $gpus --weight $weight --version 100h --test --batch_size 1 --tests RobotcarWeather --img_size $size --DATA_ROOT $data_root
   
-python test.py --gpus $gpus --weight $weight --version 100h --test --batch_size 1 --tests           KITTI --img_size $size --DATA_ROOT $data_root  
 python test.py --gpus $gpus --weight $weight --version 100h --test --batch_size 1 --tests          ETH3DI --img_size $size --DATA_ROOT $data_root  
   
 python test.py --gpus $gpus --weight $weight --version 100h --test --batch_size 1 --tests      BlendedMVS --img_size $size --DATA_ROOT $data_root  
